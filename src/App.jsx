1b344342b04a8e2d323a65765acafedefeca9d03
afika <afikashwashwa@gmail.com>
2024-01-19 21:46:43 +0200

    added Nav from bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+// import React from 'react'
 
-export const App = () => {
+ const App = () => {
   return (
-    <div>App</div>
+    <div>
+     
+    </div>
   )
 }
+export default App;
