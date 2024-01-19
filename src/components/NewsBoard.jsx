8097794ef567761af0b8b399f2789a37ce4c6c41
afika <afikashwashwa@gmail.com>
2024-01-20 01:22:47 +0200

    alt image  added and fixed positions and alignment

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -9,7 +9,7 @@ export const NewsBoard = ({category}) => {
     }, [category])
   return (
     <div>
-        <h2 className='text-center'>Latest <span className='badge bg-danger'>News</span></h2>   
+        <h1 className='text-center mt-5 '>Latest <span className='badge bg-danger'>News</span></h1>   
         {articles.map((news, index)=>{
             return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
         })}
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import image from '../assets/news.webp'
 
 export const NewsItems = ({title, description, src,url}) => {
     return (
 
         <div className="card bg-dark text-light mb-3 d-inline-block m-3 p-2" style={{maxWidth:"345px"}}>
-            <img src={src} style={{height:"200px", width:"330px"}} className="card-img-top" alt="..." />
+            <img src={src?src:image} style={{height:"200px", width:"330px"}} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">{title? title.slice(0,50): "Hot News"}</h5>
+                <h5 className="card-title">{title? title.slice(0,28): "Hot News"}</h5>
                 {/* <h5 className="card-title">{title}</h5> */}
                 <p className="card-text">{description? description.slice(0, 90): "Read this interesting story below"}</p>
                 {/* <h5 className="card-title">{title}</h5> */}
