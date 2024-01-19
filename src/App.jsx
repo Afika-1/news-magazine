ea0f8a28d4629c15135bb636668fd8c54ba38c11
afika <afikashwashwa@gmail.com>
2024-01-20 00:58:47 +0200

    Fixed navbar to navigate categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 // import React from 'react'
 
+import { useState } from "react";
 import { Navbar } from "./components/Navbar";
 import { NewsBoard } from "./components/NewsBoard";
-import { NewsItems } from "./components/NewsItems";
 
  const App = () => {
+  const [category, setCategory]= useState("general")
   return (
     <div>
-     <Navbar/>
-     <NewsBoard/>
-     <NewsItems/>
+     <Navbar setCategory={setCategory}/>
+     <NewsBoard category={category}/>
     </div>
   )
 }
--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 export const NewsItems = ({title, description, src,url}) => {
     return (
 
-        <div className="card bg-dark text-light mb-3 d-inline-block m-3 p-2" style={{height:"500px",width:"360px"}}>
-            <img src={src} style={{height:"200px", width:"340px"}} className="card-img-top" alt="..." />
+        <div className="card bg-dark text-light mb-3 d-inline-block m-3 p-2" style={{maxWidth:"345px"}}>
+            <img src={src} style={{height:"200px", width:"330px"}} className="card-img-top" alt="..." />
             <div className="card-body">
-                {/* <h5 className="card-title">{title.slice(0,50)}</h5>
-                <p className="card-text">{description? description.slice(0, 90): "Read this interesting story below"}</p> */}
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
+                <h5 className="card-title">{title? title.slice(0,50): "Hot News"}</h5>
+                {/* <h5 className="card-title">{title}</h5> */}
+                <p className="card-text">{description? description.slice(0, 90): "Read this interesting story below"}</p>
+                {/* <h5 className="card-title">{title}</h5> */}
+                {/* <p className="card-text">{description}</p> */}
                 <a href={url} className="btn btn-primary">Read More</a>
             </div>
         </div>
