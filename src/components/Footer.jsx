776abd218e50001aabc0fd1aa12665dd9ca8cd0b
afika <afikashwashwa@gmail.com>
2024-01-20 20:35:29 +0200

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,20 +5,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 export const Footer = () => {
-  return (
-    <div className='d-flex justify-content-evenly pt-3'>
-        <p>Connect with us on Social media</p>
-        <div className='d-flex gap-5 '>
-        <FontAwesomeIcon icon={faFacebookF} />
-<FontAwesomeIcon icon={faTwitter} />
-<FontAwesomeIcon icon={faInstagram} />
-<FontAwesomeIcon icon={faYoutube} />
+    return (
+        <div className='d-flex justify-content-evenly align-items-center bg-dark pt-3 fs-5 '>
+            <p className='text-white'>Connect with us on Social media</p>
+            <div className='d-flex gap-5 '>
+                <FontAwesomeIcon className='text-primary' icon={faFacebookF} />
+                <FontAwesomeIcon className='text-danger' icon={faInstagram} />
+                <FontAwesomeIcon className='text-primary text-opacity-75' icon={faTwitter} />
+                <FontAwesomeIcon className='text-danger' icon={faYoutube} />
+            </div>
+
+            <p className='text-white' ><a  className='link-underline-primary'>Subscribe</a> to our newsletter!</p>
+
+
+
+
         </div>
-
-
-
-
-        {/* <p><span>{faFacebookF}</span> <span>{faTwitter}</span> <span>{faInstagram}</span><span>{faYoutube}</span></p> */}
-    </div>
-  )
+    )
 }
--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -8,7 +8,7 @@ export const NewsBoard = ({category}) => {
         fetch(url).then(response=> response.json()).then(data=>setArticles(data.articles));
     }, [])
   return (
-    <div className='' style={{backgroundColor:" rgb(211, 211, 111)"}}>
+    <div className='pb-5' style={{backgroundColor:" rgb(211, 211, 111)"}}>
         <h1 className='text-center pt-5 '>Latest <span className='badge bg-danger'>News</span></h1>   
         {articles.map((news, index)=>{
             return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
