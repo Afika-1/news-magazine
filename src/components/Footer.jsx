2981be3a9346d162a231e9a1c5a67e7f0763127b
afika <afikashwashwa@gmail.com>
2024-01-21 14:46:03 +0200

    ...

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,10 @@ export const Footer = () => {
         <div className='d-flex justify-content-evenly align-items-center bg-dark pt-3 fs-5 '>
             <p className='text-white'>Connect with us on Social media</p>
             <div className='d-flex gap-5 '>
-                <FontAwesomeIcon className='text-primary' icon={faFacebookF} />
-                <FontAwesomeIcon className='text-danger' icon={faInstagram} />
-                <FontAwesomeIcon className='text-primary text-opacity-75' icon={faTwitter} />
-                <FontAwesomeIcon className='text-danger' icon={faYoutube} />
+               <p> <FontAwesomeIcon className='text-primary ' icon={faFacebookF} /></p>
+                <p><FontAwesomeIcon className='text-danger' icon={faInstagram} /></p>
+               <p> <FontAwesomeIcon className='text-primary text-opacity-75' icon={faTwitter} /></p>
+               <p><FontAwesomeIcon className='text-danger' icon={faYoutube} /></p> 
             </div>
 
             <p className='text-white' ><a  className='link-underline-primary'>Subscribe</a> to our newsletter!</p>
