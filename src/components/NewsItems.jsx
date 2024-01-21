f01959137cd65d9cbf02e3c707bac12b27be380b
afika <afikashwashwa@gmail.com>
2024-01-21 14:35:01 +0200

    cleaned unnnecessary code

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -8,10 +8,8 @@ export const NewsItems = ({title, description, src,url}) => {
             <img src={src?src:image} style={{height:"200px", width:"330px"}} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title? title.slice(0,28): "Hot News"}</h5>
-                {/* <h5 className="card-title">{title}</h5> */}
                 <p className="card-text" style={{minHeight:'100px'}}>{description? description.slice(0, 140): "Read this interesting story below"}</p>
-                {/* <h5 className="card-title">{title}</h5> */}
-                {/* <p className="card-text">{description}</p> */}
+               
                 <a href={url} className="btn btn-primary">Read More</a>
             </div>
         </div>
