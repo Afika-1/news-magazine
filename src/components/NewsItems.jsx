1e4079ce67cadb8bb20c28dead55a1decf5d16ba
afika <afikashwashwa@gmail.com>
2024-07-22 09:10:35 +0200

    Style and margine fixed

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -4,11 +4,11 @@ import image from '../assets/news.webp'
 export const NewsItems = ({title, description, src,url}) => {
     return (
 
-        <div className="card bg-dark text-light mb-3 d-inline-block m-3 p-2" style={{maxWidth:"345px"}}>
+        <div className="card bg-dark text-light mb-3 d-inline-block m-4 p-2" style={{maxWidth:"345px"}}>
             <img src={src?src:image} style={{height:"200px", width:"330px"}} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title? title.slice(0,28): "Hot News"}</h5>
-                <p className="card-text" style={{minHeight:'100px'}}>{description? description.slice(0, 140): "Read this interesting story below"}</p>
+                <p className="card-text" style={{minHeight:'100px'}}>{description? description.slice(0, 140): "Click on the Read More button for insigtful details about this interesting story."}</p>
                
                 <a href={url} className="btn btn-primary">Read More</a>
             </div>
