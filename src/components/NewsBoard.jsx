8070c02eae53e1992cfe8da6efff8ae27368515f
afika <afikashwashwa@gmail.com>
2024-07-27 05:36:09 +0200

    added category label

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -9,7 +9,8 @@ export const NewsBoard = ({category}) => {
     }, [category])
   return (
     <div className='pb-5' style={{backgroundColor:" rgb(211, 211, 111)"}}>
-        <h1 className='text-center pt-5 '>Latest <span className='badge bg-danger'>News</span></h1>   
+        <h1 className='text-center pt-2 '>Latest <span className='badge bg-danger'>News</span></h1> 
+        <h4 className='text-center p-3'>{category}</h4>  
         {articles.map((news, index)=>{
             return <NewsItems key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
         })}
